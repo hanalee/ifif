1d75172abcedeb780ac50c74f7caf87c47c3e8b0
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2016-05-11 19:50:03 +0900

    add warn

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
-module.exports = function(condition, message, code) {
-	if (condition) {
-		if (message) {
-			console.error(message);
-		} else {
-			var msg = 'check your ifif code, something\'s wrong..';
-			if (typeof condition === 'string') {
-				msg += '\n' + condition;
-			}
 
-			console.error(msg);
+function ifif( condition, message, errorCode, report ) {
+
+	if( arguments.length < 1 ) {
+		console.error('condition is required');
+		return;
+	}
+
+	if ( !! condition ) {
+		report( message, errorCode );
+	}
+} 
+
+
+function reportError( message, errorCode ) {
+
+	if (message) {
+
+		console.error(message);
+
+	} else {
+
+		var msg = 'check your ifif code, something\'s wrong..';
+		if (typeof condition === 'string') {
+			msg += '\n' + condition;
 		}
-		if (code) {
-			throw code;
-		} else {
-			throw 'DEFAULT_IFIF_ERROR';
-		}
+
+		console.error(msg);
+	}
+	if (errorCode) {
+		throw errorCode;
+	} else {
+		throw 'DEFAULT_IFIF_ERROR';
 	}
 }
+
+function reportWarn( message, errorCode ) {
+
+	if (message) {
+
+		console.warn(message);
+
+	} else {
+
+		var msg = 'check your ifif code, something\'s wrong..';
+		if (typeof condition === 'string') {
+			msg += '\n' + condition;
+		}
+
+		console.warn(msg);
+	}
+}
+
+var _extends = function (target) {
+	for (var i = 1; i < arguments.length; i++) {
+		var source = arguments[i];
+		for (var key in source) {
+			target[key] = source[key];
+		}
+	}
+	return target;
+}
+
+
+exports['default'] = function( condition, message, errorCode ) {
+	ifif( condition, message, errorCode, reportError );
+}
+exports.warn = function( condition, message, errorCode ) {
+	ifif( condition, message, errorCode, reportWarn );
+}
+
+module.exports = _extends(exports['default'], exports);
